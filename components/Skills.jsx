17190197d40f38912240281240b5889ebd6710c9
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-03-22 18:39:56 +0300

    Navbar changes

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -12,17 +12,6 @@ const Skills = () => {
             <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src ='/iot.webp' alt ='/' width='64' height='64'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>IoT</h3>
-                        </div>
-                    </div>
-                </div>
-
-            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
                             <Image src ='/react.png' alt ='/' width='64' height='64'/>
                         </div>
                         <div className='flex flex-col items-center justify-center'>
@@ -75,6 +64,16 @@ const Skills = () => {
                     </div>
                 </div>
 
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src ='/iot.webp' alt ='/' width='64' height='64'/>
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>IoT</h3>
+                        </div>
+                    </div>
+                </div>
 
 
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
