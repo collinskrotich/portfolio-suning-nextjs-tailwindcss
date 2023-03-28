d4b89978689431f978ebf975121755289f0a1dc4
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-03-28 12:03:13 +0300

    Added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import Navbar from '../../components/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Analytics/>
       <Navbar/>
       <Component {...pageProps} />    
     </>
